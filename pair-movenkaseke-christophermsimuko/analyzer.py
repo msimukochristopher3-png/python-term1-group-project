46559bdc45e7b3072519877dd3e9d00125bf7fa6
msimukochristopher3-png <msimukochristopher3@gmail.com>
2026-02-16 19:30:12 +0200

    Added number_analyzer function skeleton with TODOs

---
+++ pair-movenkaseke-christophermsimuko/analyzer.py
@@ -0,0 +1,14 @@
+def number_analyzer():
+    """
+    Analyze a number: determine if it's even/odd, positive/negative/zero,
+    and prime or not.
+    """
+    # TODO: Add input validation
+    # TODO: Add even/odd check
+    # TODO: Add sign check
+    # TODO: Add prime check
+    pass
+
+# Temporary test call (will be removed later)
+if __name__ == "__main__":
+    number_analyzer()
