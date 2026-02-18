2c974c6dd33dc122e5bfdaef83d4c80603893630
msimukochristopher3-png <msimukochristopher3@gmail.com>
2026-02-18 11:21:52 +0200

    craeted function skeleton

---
+++ pair-movenkaseke-christophermsimuko/word-counter.py
@@ -0,0 +1,15 @@
+def word_counter():
+    """
+    Count words, characters (excluding spaces), and find the longest word
+    in a user-provided sentence.
+    """
+    # TODO: Get sentence input
+    # TODO: Count words
+    # TODO: Count characters without spaces
+    # TODO: Find longest word
+    # TODO: Display results
+    pass
+
+# Temporary test call (can be removed later)
+if __name__ == "__main__":
+    word_counter()
